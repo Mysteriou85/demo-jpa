7f961f1c8ed28d6ec4ecd730b7179bb66c135ea9
Mysteriou85 <guib5@hotmail.fr>
2022-06-27 10:50:56 +0200

    init

---
+++ src/main/java/fr/diginamic/ConnexionJpa.java
@@ -0,0 +1,14 @@
+package fr.diginamic;
+
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
+
+public class ConnexionJpa {
+    public static void main(String[] args) {
+
+        EntityManagerFactory entityManagerFactory = Persistence.createEntityManagerFactory("pu_essai");
+        EntityManager entityManager = entityManagerFactory.createEntityManager();
+
+    }
+}
